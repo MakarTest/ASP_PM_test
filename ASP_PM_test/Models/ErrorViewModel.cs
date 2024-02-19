7532f8def2c97980837f19c7b2a92b1bae035894
Makar <mahadrum@yandex.ru>
2024-02-19 22:40:40 +0300

    Добавьте файлы проекта.

---
+++ ASP_PM_test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_PM_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
